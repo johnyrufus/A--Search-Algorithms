09627a105dbc5b747e237acef07978c3d9aea356
Johny Rufus <johnjoh@iu.edu>
2017-09-22 19:49:54 -0700

    Problem 2,  base version, updated with AssignmentState class

--- problem2/test_assign.py
+++ problem2/test_assign.py
@@ -3,53 +3,77 @@
 import unittest
 import sys
 from assign import AssignmentSolver
+from assign import AssignmentState
 from assign import Group
 from assign import User
 from unittest import TestCase
-if __name__ == '__main__':
-    unittest.main()
 
 
 class AssignmentSolverTest(TestCase):
 
     def test_basic_groups1(self):
-        print(sys.path)
         solver = AssignmentSolver
         solver.max_members_in_group = 1
-        solver.k = 160
-        solver.m = 31
-        solver.n = 10
+        solver.k, solver.m, solver.n = 160, 31, 10
         solver.initialize()
 
-        self.assertEqual(len(solver.groups), len(solver.users) + 1)
+        self.assertEqual(len(solver.state.groups), len(solver.users) + 1)
         self.assertEqual(len(solver.users), 6)
-        self.assertEqual(sorted([g.score for g in solver.groups]), [0,0,0,0,10,21,21])
-        self.assertEqual(solver.calculate_time(), 1012)
+        self.assertEqual(sorted([g.evaluate() for g in solver.state.groups]), [0,0,0,0,10,21,21])
+        self.assertEqual(solver.state.evaluate(solver.k), 1012)
 
     def test_basic_groups2(self):
-        print(sys.path)
         solver = AssignmentSolver
         solver.max_members_in_group = 3
-        solver.k = 160
-        solver.m = 31
-        solver.n = 10
+        solver.k, solver.m, solver.n = 160, 31, 10
         solver.users = []
         solver.initialize()
 
-        solver.groups = [solver.create_group()]
-        group1 = solver.create_group()
-        group2 = solver.create_group()
-        group3 = solver.create_group()
+        list1, list2, list3 = list(), list(), list()
 
         for user in solver.users:
             if user.user_id == 'djcran' or user.user_id == 'chen464':
-                group1.assign_user_to_group(user)
+                list1.append(user)
             elif user.user_id == 'kapadia' or user.user_id == 'zehzhang' or user.user_id == 'fan6':
-                group2.assign_user_to_group(user)
+                list2.append(user)
             elif user.user_id == 'steflee':
-                group3.assign_user_to_group(user)
+                list3.append(user)
 
-        self.assertEqual(len(solver.groups), 4)
+        solver.state = AssignmentState([Group(list()), Group(list1), Group(list2), Group(list3)])
+
+        self.assertEqual(len(solver.state.groups), 4)
         self.assertEqual(len(solver.users), 6)
-        self.assertEqual(sorted([g.score for g in solver.groups]), [0,0,12,42])
-        self.assertEqual(solver.calculate_time(), 534)
+        self.assertEqual(sorted([g.evaluate() for g in solver.state.groups]), [0,0,12,42])
+        self.assertEqual(solver.state.evaluate(solver.k), 534)
+
+    def test_basic_groups3(self):
+        solver = AssignmentSolver
+        solver.max_members_in_group = 3
+        solver.k, solver.m, solver.n = 160, 31, 10
+        solver.users = []
+        solver.initialize()
+
+        solver.state = AssignmentState()
+
+        solver.state = solver.state.assign_user_to_group(self.get_user('djcran', solver.users), solver.state.groups[0])
+        self.assertEqual(solver.state.groups[1].evaluate(), 21)
+        self.assertEqual(solver.state.evaluate(solver.k), 181)
+
+        solver.state = solver.state.assign_user_to_group(self.get_user('chen464', solver.users), solver.state.groups[1])
+        self.assertEqual(solver.state.evaluate(solver.k), 172)
+
+        solver.state = solver.state.assign_user_to_group(self.get_user('kapadia', solver.users), solver.state.groups[0])
+        self.assertEqual(solver.state.evaluate(solver.k), 353)
+
+        solver.state = solver.state.assign_user_to_group(self.get_user('zehzhang', solver.users), solver.state.groups[1])
+        print(solver.state.groups)
+        self.assertEqual(solver.state.evaluate(solver.k), 375)
+
+
+
+    def get_user(self, user_id, users):
+        return [user for user in users if user.user_id == user_id][0]
+
+
+if __name__ == '__main__':
+    unittest.main()
