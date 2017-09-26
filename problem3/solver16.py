cd784606180984563dbb62f0d6bac4951a052354
Falter <582139@bah.com>
2017-09-26 03:21:41 -0400

    added successor and heuristic functions

--- problem3/solver16.py
+++ problem3/solver16.py
@@ -20,22 +20,55 @@ class Node():
         self.numPrevMoves = numPrevMoves
         self.state = state
         self.move = move
-        self.estimatedDistanceToGoal = estimatedDistanceToGoal        
+        self.estimatedDistanceToGoal = estimatedDistanceToGoal   
 
-def findEmptySquare(puzzle):
+# list of goal positions for each tile + empty space
+tileGoal = [(3,3),(0,0),(0,1),(0,2),(0,3),(1,0),(1,1),(1,2),(1,3),(2,0),(2,1),(2,2),(2,3),(3,0),(3,1),(3,2)]
+successors = {}
+
+def initSuccessorDict(d):
+    '''
+    builds a set of 6 possible moves for each square on the 4x4 board
+    zero-based indexing used for location, per the python/numpy convention; however, the move encoding is one-based
+    '''
+    d[(0,0)] = ["U11","U21","U31","L11","L21","L31"]
+    d[(0,1)] = ["U12","U22","U32","L11","L21","R11"]
+    d[(0,2)] = ["U13","U23","U33","L11","R11","R21"]
+    d[(0,3)] = ["U14","U24","U34","R11","R21","R31"]
+    d[(1,0)] = ["U11","U21","D11","L12","L22","L32"]
+    d[(1,1)] = ["U12","U22","D12","L12","L22","R12"]
+    d[(1,2)] = ["U13","U23","D13","L12","R12","R22"]
+    d[(1,3)] = ["U14","U24","D14","R12","R22","R32"]
+    d[(2,0)] = ["U11","D11","D21","L13","L23","L33"]
+    d[(2,1)] = ["U12","D12","D22","L13","L23","R13"]
+    d[(2,2)] = ["U13","D13","D23","L13","R13","R23"]
+    d[(2,3)] = ["U14","D14","D24","R13","R23","R33"]
+    d[(3,0)] = ["D11","D21","D31","L14","L24","L34"]
+    d[(3,1)] = ["D12","D22","D32","L14","L24","R14"]
+    d[(3,2)] = ["D13","D23","D33","L14","R14","R24"]
+    d[(3,3)] = ["D14","D24","D34","R14","R24","R34"]
+   
+        
+def findTile(val, puzzle):
     '''
     puzzle = ndarray(4,4) with elements in range(15)
-    returns tuple indicating (row, column) where the zero element is located
+    returns tuple indicating (row, column) where the tile having val is located
     '''
-    locationZero = np.where(0 == puzzle)
-    return (locationZero[0][0], locationZero[1][0])
+    location = np.where(val == puzzle)
+    return (location[0][0], location[1][0])
 
-def getEstimatedDistanceToGoal(puzzle):
+def estimateDistance(puzzle):
     '''
     puzzle = ndarray(4,4) with elements in range(15)
-    heuristic function for estimating distance to goal state
+    heuristic function for estimating distance to goal state = 
+    Sum of Manhattan distance for each tile from its goal position
     '''
-    pass
+    dist = 0
+    for i in range(1,16):
+        goal = tileGoal[i]
+        actual = findTile(i, puzzle)
+        dist += (abs(goal[0] - actual[0]) + abs(goal[1] - actual[1]))
+    return dist / 3   
     
 def isSolvable(puzzle):
     '''
@@ -53,7 +86,40 @@ def isSolvable(puzzle):
                 continue
             if puzzle[m] < n:
                 parity = parity + 1
-                print str(puzzle[m]) + " < " + str(n)
     return (parity % 2 == 0)
-            
+
+def swapTiles(pos1, pos2, puzzle):
+    '''
+    swap the tiles that are in position 1 and position 2
+    '''
+    puzzle[pos1], puzzle[pos2] = puzzle[pos2], puzzle[pos1]
+
+def getNewZeroPosition(oldZero, direction):
+    newRow = oldZero[0] if direction in ['R','L'] else oldZero[0] - 1 if direction == 'D' else oldZero[0] + 1
+    newCol = oldZero[1] if direction in ['D','U'] else oldZero[1] - 1 if direction == 'R' else oldZero[1] + 1
+    return tuple([newRow,newCol])
+           
+def getSuccessors(node):
+    '''
+    return an unordered list of the 6 successors to the puzzle state passed as arg
+    '''
+    successors = []
+    locationZero = findTile(0, node.state)
+    for move in successors[locationZero]:
+        newPuzzle = node.state.copy()
+        newZero = locationZero
+        direction = move[0]
+        numTiles = move[1]
+        for i in range(numTiles):
+            oldZero = newZero
+            newZero = getNewZeroPosition(oldZero, direction)
+            swapTiles(oldZero, newZero, newPuzzle)
+        successor = Node(node, node.numPrevMoves + 1, newPuzzle, move, estimateDistance(newPuzzle))
+        successors.append(successor)
+    return successors
+                
+                
+                
+        
+        
             
--- problem3/testSolver16.py
+++ problem3/testSolver16.py
@@ -4,15 +4,16 @@ Authors: Chris Falter, Johny Rufus John, and Xing Liu
 Test the 15-tile puzzle solver code
 """
 import unittest
-from solver16 import findEmptySquare, isSolvable
+from solver16 import findTile, isSolvable, estimateDistance
 import numpy as np
 
 class TestSolver16(unittest.TestCase):
     
-    def test_findEmptySquare_ReturnsLocationOfZeroValueElement(self):
+    def test_findTile_ReturnsCorrectLocation(self):
         puzzle = np.array([[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]])
-        locationOfZero = findEmptySquare(puzzle)
-        self.assertEquals(locationOfZero, (0,0))
+        argToLocation = {0:(0,0), 1:(0,1), 2:(0,2), 3:(0,3), 5:(1,1), 10:(2,2), 14:(3,2), 15:(3,3)}
+        for arg, location in argToLocation.items():
+            self.assertEquals(findTile(arg, puzzle), location)
         
     def test_isSolvable_WithUnsolvableState_ReturnsFalse(self):
         puzzle = np.array([[2,1,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]])
@@ -21,6 +22,15 @@ class TestSolver16(unittest.TestCase):
     def test_isSolvable_WithSolvableState_ReturnsTrue(self):
         puzzle = np.array([[1,2,3,4],[7,5,6,12],[8,9,10,11],[13,14,15,0]])
         self.assertTrue(isSolvable(puzzle))
+        
+    def test_estimateDistance_ReturnsTotalManhattanDistance(self):
+        puzzle0 = np.array([[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]])
+        puzzle1 = np.array([[2,1,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]])
+        puzzle2 = np.array([[1,2,3,4],[7,5,6,12],[8,9,10,11],[13,14,15,0]])
+        distToPuzzle = {8:puzzle0, (2/3):puzzle1, 4:puzzle2}
+        for dist, puzzle in distToPuzzle.items():
+            self.assertEquals(estimateDistance(puzzle), dist)        
+        
 
 if __name__ == '__main__':
     unittest.main()
