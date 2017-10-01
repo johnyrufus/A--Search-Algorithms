3708939616ff15b9367f18e7d9b4b2bb9f6cd7fe
xingliu <xingliu@149-161-198-13.dhcp-bl.indiana.edu>
2017-10-01 18:05:22 -0400

    move comments into source code file

--- problem1/route.py
+++ problem1/route.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-# Xing Liu
+# Problem 1 Author: Xing Liu
+# Comments: Folder 'geopy' inside problem 1 is the library for calculating great-circle distance, 
+# I found that the department linux server does not have this library installed so I included it. 
+# Thanks Chris and Johny for providing a rough code structure, although I decided to start everything 
+# from scratch later on but I would like to mention their efforts on problem 1.
 
 # (1) If just considering the minimal cost, for routing option requiring minimal segments change, 
 # the BFS/UNIFORM/ASTAR are equally good, for routing option requiring minimal total distance and
